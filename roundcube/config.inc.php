7f5b7b500282eef5d54ef4bb6d6078cc0aaabbe1
Remo Stäuble <remo.staeuble@infoguard.ch>
2018-07-25 14:27:12 +0200

    Adds Roundcube for webmail

---
+++ roundcube/config.inc.php
@@ -0,0 +1,7 @@
+<?php
+$config['db_dsnw'] = 'sqlite:////var/www/html/db/sqlite.db?mode=0646';
+$config['db_dsnr'] = '';
+$config['default_host'] = 'imap';
+$config['default_port'] = '143';
+$config['smtp_server'] = 'imap';
+$config['smtp_port'] = '25';
